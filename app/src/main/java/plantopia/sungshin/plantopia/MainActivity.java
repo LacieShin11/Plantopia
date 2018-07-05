f2a978e42c0edf19072f33ae1f8959446c0e9a97
yunji <zion830@naver.com>
2018-07-06 04:56:56 +0900

    로그인 화면 레이아웃

---
+++ app/src/main/java/plantopia/sungshin/plantopia/MainActivity.java
@@ -0,0 +1,13 @@
+package plantopia.sungshin.plantopia;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
